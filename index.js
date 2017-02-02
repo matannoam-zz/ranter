acfc0ed3684ca7db23ccdd6a56c675db08e4313c
Matan Noam Shavit <matannoam@gmail.com>
2017-02-02 18:20:09 -0500

    serve an html page over express

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ var app = require('express')()
 var http = require('http').Server(app)
 
 app.get('/', function(request, response) {
-    response.send(
-        '<h1>Ranter</h1><h2>A place to rant</h2>'
-    )
+    response.sendFile(__dirname + '/index.html')
 })
+
 
 http.listen(3000, function(){
     console.log('server listening on *:3000')
