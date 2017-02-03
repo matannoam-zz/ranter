ec52024a3ca3e66ac9e37058feb26e39f75f5b1e
Matan Noam Shavit <matannoam@gmail.com>
2017-02-02 21:57:25 -0500

    socket io events and responses

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var app = require('express')()
 var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
 app.get('/', function(request, response) {
     response.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', function(socket){
+    socket.on('chat message', function(message){
+        io.emit('chat message', message)
+    })
 })
 
 
